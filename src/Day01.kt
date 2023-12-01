4b68640512b32c0c2a035f8aa0853f2f262be2f1
Juanjo Marti <juanjo.marti@colba.es>
2023-12-01 17:56:50 +0100

    First Day 🌟🌟

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,14 +1,24 @@
 fun main() {
 
-    fun getNumber(result: String): Int{
-        if (result.length == 1) {
-            return Integer.parseInt(result + result)
-        } else if (result.length == 2) {
-            return Integer.parseInt(result)
-        } else {
-            val first = result.first().toString()
-            val last = result.last().toString()
-            return Integer.parseInt(first + last)
+    fun getNumber(result: String): Int {
+        return when (result.length) {
+            0 -> {
+                0
+            }
+
+            1 -> {
+                Integer.parseInt(result + result)
+            }
+
+            2 -> {
+                Integer.parseInt(result)
+            }
+
+            else -> {
+                val first = result.first().toString()
+                val last = result.last().toString()
+                Integer.parseInt(first + last)
+            }
         }
     }
 
@@ -22,17 +32,76 @@ fun main() {
         return total
     }
 
+    fun findFirstDigit(row: String): String{
+        var word = ""
+        row.forEach { letter ->
+            word += letter
+            if(letter.isDigit()) return letter.toString()
+            with(word) {
+                when {
+                    contains("one") -> return "1"
+                    contains("two") -> return "2"
+                    contains("three") -> return "3"
+                    contains("four") -> return "4"
+                    contains("five") -> return "5"
+                    contains("six") -> return "6"
+                    contains("seven") -> return "7"
+                    contains("eight") -> return "8"
+                    contains("nine") -> return "9"
+                }
+            }
+        }
+        return ""
+    }
+
+    fun findLastDigit(row: String): String{
+        var word = ""
+        row.reversed().forEach { letter ->
+            word += letter
+            if(letter.isDigit()) return letter.toString()
+            with(word) {
+                when {
+                    contains("one".reversed()) -> return "1"
+                    contains("two".reversed()) -> return "2"
+                    contains("three".reversed()) -> return "3"
+                    contains("four".reversed()) -> return "4"
+                    contains("five".reversed()) -> return "5"
+                    contains("six".reversed()) -> return "6"
+                    contains("seven".reversed()) -> return "7"
+                    contains("eight".reversed()) -> return "8"
+                    contains("nine".reversed()) -> return "9"
+                }
+            }
+        }
+        return ""
+    }
+
+    fun translateRow(row: String): String{
+        val firstDigit = findFirstDigit(row)
+        println("First Digit Found --> $firstDigit")
+        val lastDigit = findLastDigit(row)
+        println("Last Digit Found --> $lastDigit")
+        return (firstDigit + lastDigit)
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        var total = 0
+        input.forEach { row ->
+            val result = translateRow(row)
+            println("Translated String = $result")
+            println("example = ${getNumber(result)}")
+            total += getNumber(result)
+        }
+        println("TOTAL --> $total")
+        return total
     }
 
 
-
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 142)
+    val testInput = readInput("Day01")
+    if (part2(testInput) == 281) println("✅") else println("❌")
 
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+//    part1(input).println()
+//    part2(input).println()
 }
