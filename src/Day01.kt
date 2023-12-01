b2519fe09752583ad19f37edf391e68b5890816c
Juanjo Marti <juanjo.marti@colba.es>
2023-12-01 16:10:16 +0100

    First Start 🌟

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,36 @@
 fun main() {
+
+    fun getNumber(result: String): Int{
+        if (result.length == 1) {
+            return Integer.parseInt(result + result)
+        } else if (result.length == 2) {
+            return Integer.parseInt(result)
+        } else {
+            val first = result.first().toString()
+            val last = result.last().toString()
+            return Integer.parseInt(first + last)
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var total = 0
+        input.forEach { row ->
+            val result = row.filter { it.isDigit() }
+            total += getNumber(result)
+        }
+        println("TOTAL --> $total")
+        return total
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
+
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
