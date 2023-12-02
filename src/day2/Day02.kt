c90127c4af21eb8e92397ba667b8a54d606a906b
Juanjo Marti <juanjo.marti@colba.es>
2023-12-02 17:58:08 +0100

    Solution Day2 Part 2 🌟🌟

--- src/day2/Day02.kt
+++ src/day2/Day02.kt
@@ -12,7 +12,6 @@ fun main() {
 class Day01(private val input: List<String>) {
 
     fun solvedPart1(): Int {
-
         var total = 0
         input.forEachIndexed { index, s ->
             var isGameValid = true
@@ -39,7 +38,39 @@ class Day01(private val input: List<String>) {
     }
 
     fun solvedPart2(): Int {
-        return 2
+        var solution = 0
+        input.forEachIndexed { index, s ->
+            val games = s.split(":")
+            val turn = games[1].split(";")
+            val targetList: MutableList<Pair<Int, String>> = mutableListOf()
+            var totalGame = 1
+            turn.forEach {
+                val options = it.split(",")
+                options.forEach { option ->
+                    val input = option.split(" ")
+                    if (targetList.isEmpty() || targetList.find { item -> item.second == input[2] } == null) {
+                        targetList.add(Pair(input[1].toInt(), input[2]))
+                    } else {
+                        val tempTargetList: MutableList<Pair<Int, String>> = mutableListOf()
+                        targetList.forEach { target ->
+                            if (input[2] == target.second && input[1].toInt() > target.first) {
+                                tempTargetList.add(Pair(input[1].toInt(), input[2]))
+                            }
+                        }
+                        if (tempTargetList.isNotEmpty()) {
+                            val test = targetList.find { it.second == tempTargetList[0].second }
+                            val id = targetList.indexOf(test)
+                            targetList.set(id, tempTargetList[0])
+                        }
+                    }
+                }
+            }
+            targetList.forEach {
+                totalGame *= it.first
+            }
+            solution += totalGame
+        }
+        return solution
     }
 
 }
